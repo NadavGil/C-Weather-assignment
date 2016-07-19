c515ed6b48fae9b07a40e0e2b32fe72458b3cc68
Nadav Gilron <nadav.g@Taptica.local>
2016-07-19 17:09:41 +0300

    full assignment

---
+++ TDD/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherAssignment;
+
+namespace TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetWeatherData()
+        {
+            WeatherDataServiceFactory obj = WeatherDataServiceFactory.Instance;
+
+            CityWeather tmp = obj.GetWeatherDataService("London");                  //check if get worked
+            Assert.IsNotNull(tmp);
+        }
+
+        [TestMethod]
+        public void Test2GetWeatherData()
+        {
+            WeatherDataServiceFactory obj = WeatherDataServiceFactory.Instance;
+            CityWeather tmp = obj.GetWeatherDataService("Lahore");                  //check if get worked
+            Assert.IsNotNull(tmp);
+        }
+    }
+}
